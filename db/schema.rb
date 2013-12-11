a4ca05185273e03cd4639e4a800a00c2318bd77c
Jason Vasquez <fjvasquez@aol.com>
2013-12-11 15:28:18 -0800

    users update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211195024) do
+ActiveRecord::Schema.define(version: 20131211231347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20131211195024) do
     t.float    "weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string "username"
+    t.string "password"
   end
 
 end
