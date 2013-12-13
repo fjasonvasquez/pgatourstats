728af971b2fea0c622db6c6a38048c4f44dff2ba
Jason Vasquez <fjvasquez@aol.com>
2013-12-13 14:39:34 -0800

    Users working

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Pga::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your  site routed with "root"
   root to: 'users#new'
-
+  resources :players
   resources :users, only: [:new, :create, :index]
   delete '/users/new' => 'authentications#destroy'
 
